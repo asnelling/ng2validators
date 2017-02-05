52c1ff2319b7adb71d0a1f0a5bc9397605835510
Addison Snelling <asnell@utexas.edu>
2017-02-05 06:52:37 -0600

    don't need all this NgModule mess

--- index.ts
+++ index.ts
@@ -1,2 +1,19 @@
-export {Ng2ValidatorsModule} from "./src/validators.module";
-export {Ng2Validators} from "./src/validators";
+import {ValidatorFn, AbstractControl} from "@angular/forms";
+
+export class Ng2Validators {
+
+  /**
+   * Among the children of `control` identified in `controls`, at least
+   * `numTrue` must have truthish values.
+   * @param numTrue
+   * @param controls
+   * @returns {(control:AbstractControl)=>{[p: string]: any}}
+   */
+  static someTrue(numTrue: number, ...controls: string[]): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} => controls
+      .filter(name => !!control.get(name).value).length < numTrue
+      ? {someTrue: controls}
+      : null;
+  }
+
+}
--- src/validators.module.ts
+++
@@ -1,8 +0,0 @@
-import {NgModule} from "@angular/core";
-import {Ng2Validators} from "./validators";
-
-@NgModule({
-  declarations: [Ng2Validators], exports: [Ng2Validators]
-})
-export class Ng2ValidatorsModule {
-}
